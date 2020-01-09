44dff9e322a28549c9aa54689326f553764145a9
conor-v <conor.vanoystaeyen@student.howest.be>
2020-01-09 16:49:27 +0100

    heroku DB bug2

--- src/dao/humoDAO.php
+++ src/dao/humoDAO.php
@@ -3,7 +3,7 @@
 require_once( __DIR__ . '/DAO.php');
 
 class HumoDAO extends DAO {
-  
+
   public function selectAll(){
     $sql = "SELECT * FROM `shop_items`";
     $stmt = $this->pdo->prepare($sql);
