56fc290a4106e335c2b626a66b1cb748c41803af
conor-v <conor.vanoystaeyen@student.howest.be>
2020-01-13 21:56:34 +0100

    producten in laden fixes #4

--- src/view/humo/index.php
+++ src/view/humo/index.php
@@ -1,14 +1,14 @@
 <section class="promo">
-  <h1 class="promo__title">promo actie: boek van de week</h1>
+  <h2 class="promo__title">promo actie: boek van de week</h2>
   <img src="../assets/promoboek.png" alt="promo boek">
-  <h2 class="promo__boek__title">Farenheit 451 Ray Bradbury</h2>
+  <h3 class="promo__boek__title">Farenheit 451 Ray Bradbury</h3>
   <div class="promo__info">
     <p class="promo__info__price">&euro;12,99</p>
     <p class="promo__info__slogan">100% lees plezier</p>
   </div>
   <div class="promo__buttons">
     <a class="promo__carbutton" href="#"><img src="../assets/cart.png" alt="cart">+</a>
-    <a class="promo__moreinfo" href="index.php?page=details">meer info</a>
+    <a class="promo__moreinfo" href="index.php?page=details&amp;id=3">meer info</a>
   </div>
   <div class="promo__action">
     <p class="promo__action__text">Met <span>humo code</span> is het nog maar &euro;4,99</p>
@@ -17,5 +17,25 @@
 </section>
 
 <section>
+  <h2 class="hidden">filter producten</h2>
   
+</section>
+
+<section class="product__container">
+  <h2 class="hidden">producten</h2>
+  <?php foreach($items as $item): ?>
+    <article class="product">
+      <div class="product__img__container">
+        <img class="product__img" src="<?php echo $item['foto'] ?>" alt="<?php $item['naam'] ?>">
+      </div>
+      <div class="product__detials">
+        <h3 class="product__title"><?php echo $item['naam'] ?></h3>
+        <p class="product__price"><?php echo '&euro;' . $item['prijs'] ?></p>
+        <div class="promo__buttons product__buttons">
+          <a class="promo__carbutton" href="#"><img src="../assets/cart.png" alt="cart">+</a>
+          <a class="promo__moreinfo" href="index.php?page=details&amp;id=<?php echo $item['id'] ?>">meer info</a>
+        </div>
+      </div>
+    </article>
+  <?php endforeach; ?>
 </section>
